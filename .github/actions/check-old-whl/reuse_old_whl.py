6e26978f9dc406b66f979722966594db041e8a4f
Catherine Lee <csl@fb.com>
2025-05-13 21:41:14 -0700

    tc

--- .github/actions/check-old-whl/reuse_old_whl.py
+++ .github/actions/check-old-whl/reuse_old_whl.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Optional
 import argparse
 import zipfile
+import shutil
 
 import requests
 
@@ -125,9 +126,7 @@ def unzip_artifact_and_replace_files():
                 file_path = os.path.join(root, file)
                 zip_ref.write(file_path, os.path.relpath(file_path, "/tmp/artifacts"))
     # move artifact to the current directory
-    if os.path.exists("artifacts.zip"):
-        os.remove("artifacts.zip")
-    os.rename("/tmp/artifacts.zip", "artifacts.zip")
+    shutil.copyfile("/tmp/artifacts.zip", "artifacts.zip")
     return None
 
 
